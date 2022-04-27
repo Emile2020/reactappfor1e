fe96fa8e8877023d75bbe7dea112238385ed824f
Emile2020 <emiledebruyckere@gmail.com>
2022-04-27 16:54:21 +0200

    version 1.0! Added json request, fixed empty page, added how to learn. Ready for release!

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,42 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
-
+import axios from 'axios'
+const getnewesturl = "https://jsons.emile2021coding.repl.co/test.json"
+const getnewest = () => {
+   return axios.get(getnewesturl).then(respone => {
+    let text = respone.data.daily
+    document.getElementById("daily").innerHTML = text
+  })
+}
 function App() {
   const [count, setCount] = useState(0)
-
+  getnewest()
   return (
     <div className="App">
       <header className="App-header">
-        <undefined   setCount={setCount} count={count}  />
-        <whatis     />
-        <startjourney     />
-      </header>
-    </div>
-  )
-}
-
-export default App
-
-    function navbar({}) {
-      return (<div className='navbar'>
+      <img src='https://media3.giphy.com/media/iFmw13LV1hHhViPPWz/giphy.gif?cid=790b761119325c966fcf3fa30906fa9248b993ce5bf41708&rid=giphy.gif&ct=s' className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <div className='navbar'>
           <a href="#What is react?">What is react?</a>
           <a href="#What is vite?">What is vite?</a>
           <a href="#start your journey">How do I start?</a>
-        </div>);
-    }
-
-    function undefined({setCount, count}) {
-      return (<><img src='https://media3.giphy.com/media/iFmw13LV1hHhViPPWz/giphy.gif?cid=790b761119325c966fcf3fa30906fa9248b993ce5bf41708&rid=giphy.gif&ct=s' className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <navbar />
+        </div>
         <p>
           <button type="button" onClick={() => setCount(count => count + 1)}>
             count is: {count}
           </button>
-        </p></>);
-    }
+        </p>
+        <p>
+          <br id='What is react?' />
+          <h1><strong>What is react?</strong></h1>
+          <ul><li>React is a free framework that is popular because it's the easiest out there!.</li></ul>
 
-    function startjourney({}) {
-      return (<p id='start your journey'>
+        </p>
+        <p>
+          <br id='What is vite?' />
+          <h1><strong>What is vite?</strong></h1>
+          <ul><li>Vite is a framework that is easy and has a function that automatically updates when you are coding!</li></ul>
+        </p><p id='start your journey'>
           <br />
           <h2><em>Want to start your journey in making sites?</em></h2>
           <h3>Below are some good places too start learning!</h3>
@@ -65,20 +63,11 @@ export default App
               </ul>
             </li>
           </ul>
-        </p>);
-    }
+        </p> <h1>Newest text from me!</h1>
+        <div id='daily'></div>
 
-    function whatis({}) {
-      return (<><p>
-          <br id='What is react?' />
-          <h1><strong>What is react?</strong></h1>
-          <ul><li>React is a free framework that is popular because it's the easiest out there!.</li></ul>
-
-        </p>
-        <p>
-          <br id='What is vite?' />
-          <h1><strong>What is vite?</strong></h1>
-          <ul><li>Vite is a framework that is easy and has a function that automatically updates when you are coding!</li></ul>
-        </p></>);
-    }
-        
+      </header>
+    </div>
+  )
+}
+    export default App
